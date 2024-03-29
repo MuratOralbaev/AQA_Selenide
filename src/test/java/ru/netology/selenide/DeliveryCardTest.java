75dfa0cb8e526b55c9c4a626468e01c9bb9715a0
Murat Oralbaev <successmanonly@gmail.com>
2024-03-29 22:04:05 +0300

    DeliveryCardTest.java modified

--- src/test/java/ru/netology/selenide/DeliveryCardTest.java
+++ src/test/java/ru/netology/selenide/DeliveryCardTest.java
@@ -1,32 +1,32 @@
 package ru.netology.selenide;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Configuration.holdBrowserOpen;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class DeliveryCardTest {
-    public static String getLocalDate(long daysToAdd){
-        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("ru")));
-    }
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999/");
-        holdBrowserOpen = true;
+    private String getLocalDate(int daysToAdd, String pattern){
+        return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
     @Test
     void shouldVerifyFields(){
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(getLocalDate(5, "dd.MM.yyyy"));
+        $("[data-test-id='name'] input").setValue("Петров Василий");
+        $("[data-test-id='phone'] input").setValue("+79123456789");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(".notification__title").shouldHave(visible.text("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(visible.text("Встреча успешно забронирована на " +
+                getLocalDate(5, "dd.MM.yyyy")), Duration.ofSeconds(15));
 
     }
-
 
 }
